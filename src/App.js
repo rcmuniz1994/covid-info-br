ca88199649928c8266082ffc51a00bb9058f4bb9
Rodrigo Muniz <rodrigomuniz.gppcom@gmail.com>
2020-06-21 01:16:19 -0300

    move Brazil general info cards to another file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
-import moment from "moment";
-import "moment/locale/pt-br";
 import CovidCasesTable from "./CovidCasesTable";
+import GeneralInfoCards from "./GeneralInfoCards";
 import "./App.css";
 
 function App() {
@@ -27,64 +25,7 @@ function App() {
   return (
     <Container>
       <h1>COVID-19: Brasil</h1>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <strong>Atualizado em:</strong>
-              </Card.Title>
-              <Card.Text>
-                {`${moment(covidInfoBr.updated_at)
-                  .locale("pt-br")
-                  .format("L")} - ${moment(covidInfoBr.updated_at)
-                  .locale("pt-br")
-                  .format("LT")}`}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <strong>Confirmados:</strong>
-              </Card.Title>
-              <Card.Text>{`${covidInfoBr.confirmed}`}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <strong>Em tratamento:</strong>
-              </Card.Title>
-              <Card.Text>{`${covidInfoBr.cases}`}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <strong>Recuperados:</strong>
-              </Card.Title>
-              <Card.Text>{`${covidInfoBr.recovered}`}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <strong>Mortes:</strong>
-              </Card.Title>
-              <Card.Text>{`Mortes: ${covidInfoBr.deaths}`}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      <GeneralInfoCards covidInfoBr={covidInfoBr} />
       <Row className="covid-table">
         <Col>
           <CovidCasesTable covidInfoPerState={covidInfoPerState} />
---
+++ src/GeneralInfoCards.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import moment from "moment";
+import "moment/locale/pt-br";
+
+const GeneralInfoCards = ({ covidInfoBr, ...props }) => {
+  return (
+    <Row>
+      <Col>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <strong>Atualizado em:</strong>
+            </Card.Title>
+            <Card.Text>
+              {`${moment(covidInfoBr.updated_at)
+                .locale("pt-br")
+                .format("L")} - ${moment(covidInfoBr.updated_at)
+                .locale("pt-br")
+                .format("LT")}`}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <strong>Confirmados:</strong>
+            </Card.Title>
+            <Card.Text>{`${covidInfoBr.confirmed}`}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <strong>Em tratamento:</strong>
+            </Card.Title>
+            <Card.Text>{`${covidInfoBr.cases}`}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <strong>Recuperados:</strong>
+            </Card.Title>
+            <Card.Text>{`${covidInfoBr.recovered}`}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <strong>Mortes:</strong>
+            </Card.Title>
+            <Card.Text>{`Mortes: ${covidInfoBr.deaths}`}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default GeneralInfoCards;
