67272baa332cdf301f368947d57e494ab5f41f23
Rodrigo Muniz <rodrigomuniz.gppcom@gmail.com>
2020-06-22 00:50:55 -0300

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import { retrievedCovidInfoPerState } from "./ducks/covidInfoPerStateSlice";
 import { retrievedCovidInfoBr } from "./ducks/covidInforBrSlice";
@@ -15,6 +16,7 @@ function App() {
   );
   const covidInfoBr = useSelector((state) => state.covidInfoBr.values);
   const stateName = useSelector((state) => state.fiteringState.stateName);
+  const [selectedState, setSelectedState] = React.useState(null);
 
   React.useEffect(() => {
     dispatch(retrievedCovidInfoPerState());
@@ -31,10 +33,37 @@ function App() {
       <CovidForm />
       <CovidCasesTable
         className="covid-table"
+        setSelectedState={setSelectedState}
         covidInfoPerState={filteredInfo}
       />
+      {selectedState && (
+        <StateInfoDialog
+          selectedState={selectedState}
+          onClose={() => setSelectedState(null)}
+        />
+      )}
     </Container>
   );
 }
+
+const StateInfoDialog = ({ selectedState, onClose, ...props }) => {
+  const covidInfoPerState = useSelector(
+    (state) => state.covidInfoPerState.items
+  );
+  const stateInfo = covidInfoPerState.filter((s) => s.uf === selectedState);
+  return (
+    <Modal show onHide={onClose} animation={true}>
+      <Modal.Header closeButton>
+        <Modal.Title>{stateInfo[0].state}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{`Casos confirmados: ${stateInfo[0].cases}`}</p>
+        <p>{`Casos descartados: ${stateInfo[0].refuses}`}</p>
+        <p>{`Casos suspeitos: ${stateInfo[0].suspects}`}</p>
+        <p>{`Mortes: ${stateInfo[0].deaths}`}</p>
+      </Modal.Body>
+    </Modal>
+  );
+};
 
 export default App;
