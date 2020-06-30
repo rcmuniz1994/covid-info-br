b0ed5b9ad27f56fe4c8ddf9b3a163519feb48b30
Rodrigo Muniz <rodrigomuniz.gppcom@gmail.com>
2020-06-30 20:55:18 -0300

    Updates

---
+++ precache-manifest.f2e7b1b899bca025f196b25b2639fecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4fb5ad40e05bb0f559c3cbb3c506d6",
+    "url": "/covid-info-br/index.html"
+  },
+  {
+    "revision": "69608b39e0febb5cdef7",
+    "url": "/covid-info-br/static/css/main.c9c48bd2.chunk.css"
+  },
+  {
+    "revision": "bd9f729716675f70000b",
+    "url": "/covid-info-br/static/js/2.070c666e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-info-br/static/js/2.070c666e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69608b39e0febb5cdef7",
+    "url": "/covid-info-br/static/js/main.5db75a7b.chunk.js"
+  },
+  {
+    "revision": "754c1e90b6166a351c7b",
+    "url": "/covid-info-br/static/js/runtime-main.aedfb184.js"
+  },
+  {
+    "revision": "ce09ee7114e1a82303167b3a544900ef",
+    "url": "/covid-info-br/static/media/virus-4835301-1920.ce09ee71.jpg"
+  }
+]);
